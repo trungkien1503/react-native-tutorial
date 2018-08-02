637260cc52b1b6b788099e8153941e204e729793
Phan Trung Kien <trungkien.phan1991@gmail.com>
2018-08-02 15:22:14 +0700

    Display image with react native

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { AppRegistry, Image } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+export default class Bananas extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
-      <View>
-        <Text>Hello world!!!</Text>
-      </View>
+      <Image source={pic} style={{width: 193, height: 110}}/>
     );
   }
 }
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => Bananas);
