b759f62511369a84af457ee875833600b0e4c654
Phan Trung Kien <trungkien.phan1991@gmail.com>
2018-08-20 13:59:09 +0700

    WebView example

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, WebView, StyleSheet, View } from 'react-native';
 
-export default class SectionListBasics extends Component {
+export default class MyWebView extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <SectionList
-          sections={[
-            {title: 'D', data: ['Devin']},
-            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-          keyExtractor={(item, index) => index}
-        />
+        <WebView
+            source = {{ uri:
+               'https://vidia.com.vn/' }}
+         />
       </View>
     );
   }
@@ -24,21 +19,7 @@ const styles = StyleSheet.create({
    flex: 1,
    paddingTop: 22
   },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
 })
 
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => SectionListBasics);
+AppRegistry.registerComponent('AwesomeProject', () => MyWebView);
